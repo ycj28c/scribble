068d607cab5fb6dd3efd0f5e0f7444954ba2c6df
ycj28c <cyangjob@gmail.com>
2017-04-01 00:06:52 -0700

    commit_time

--- auto.bat
+++ auto.bat
@@ -6,8 +6,9 @@ git add .
 
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%a-%%b-%%c)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a:%%b)
-SET commit_time = %mydate%-%mytime%
+SET commit_time = %mydate-%mytime
+echo %commit_time%
 
-git commit -m commit_time
+git commit -m %commit_time
 
 git push origin master:master
