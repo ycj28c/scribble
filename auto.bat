26a96d5e82556b0f8bea338a68563e61c27c54a8
ycj28c <cyangjob@gmail.com>
2017-03-31 23:58:22 -0700

    周五-03-31_2358

--- auto.bat
+++ auto.bat
@@ -1,4 +1,9 @@
 git pull
 git add .
-git commit -m "update"
+
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+
+
+git commit -m %mydate%_%mytime%
 git push origin master:master
