2e285c5985f276716b6b7853d6bb337dd94e68ab
ycj28c <cyangjob@gmail.com>
2017-04-01 00:01:47 -0700

    commit_time

--- auto.bat
+++ auto.bat
@@ -1,11 +1,13 @@
 chcp 437
 
 git pull
+
 git add .
 
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%a-%%b-%%c)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a:%%b)
+SET commit_time = %mydate%-%mytime%
 
+git commit -m commit_time
 
-git commit -m %mydate%_%mytime%
 git push origin master:master
